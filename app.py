28fdc28c845c15f628426de67585a79c91690451
Kachukwu Okoh <okohkachukwu@gmail.com>
2022-06-05 00:55:24 +0100

    update app

--- app.py
+++ app.py
@@ -489,12 +489,28 @@ def create_show_submission():
   # called to create new shows in the db, upon submitting new show listing form
   # TODO: insert form data as a new Show record in the db, instead
 
-  # on successful db insert, flash success
-  flash('Show was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Show could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-  return render_template('pages/home.html')
+  trace = request.form.get
+  deets = ArtistForm(request.form)
+  if deets.validate():
+    try:
+      artist_id = trace('artist_id')
+      venue_id = trace('venue_id'),
+      artist = Artist.query.get(artist_id)
+      venue_name = Venue.query.get(venue_id)
+      show = Show(artist_id=artist_id, venue_id=venue_id,
+                  show_time=trace('start_time'))
+      db.session.add(show)
+      db.session.commit()
+      # on successful db insert, flash success
+      flash('Artist ' + request.form['name'] + ' was successfully listed!')
+    except:
+      db.session.rollback()
+      # on unsuccessful db insert, flash an error instead.
+      flash('ERROR!!!' + request.form['name'] + ' could not be listed.')
+    return redirect(url_for('index'))
+  else:
+    flash('Please do a re-check on form input')
+    return redirect(url_for('index'))
 
 @app.errorhandler(404)
 def not_found_error(error):
